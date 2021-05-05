817357952fe9a0e0c9f486de282851ffeda3b664
Kiandry Pauwels <kiandry.pauwels@student.ap.be>
2021-05-05 14:24:56 +0200

    Rest API command to pathc a user profile

--- snippets.ps1
+++ snippets.ps1
@@ -28,3 +28,41 @@ git push -u origin main
 git commit -m "First PowershellForGithub commands"
 git push -u origin main
 Get-History
+Get-History | Set-Content .\snippets.ps1
+Get-Content .\snippets.ps1
+git add .\snippets.ps1
+git commit -m "Added extra repo commands"
+git push -u origin main
+Get-GitHubConfiguration
+cls
+Get-Help Invoke-RestMethod
+'{"login": "med12", "group": "students"}' | ConvertFrom-Json
+Get-GitHubUser -Current
+$headershashmap='{"login": "KiandryPauwelsAP", "group":"students"}'
+$headershashmap
+$headershashmap | ConvertFrom-Json
+Invoke-RestMethod -Headers $headers https://api.github.com
+Invoke-RestMethod -Headers $headers https://api.github.com/KiandryPauwelsAP
+Invoke-RestMethod -Headers $headers https://api.github.com/user
+$auth = [System.Consolenvert]::ToBase64String([char[]]$creds.GetNetworkCredentail().Password)
+$auth = [System.Convert]::ToBase64String([char[]]$creds.GetNetworkCredentail().Password)
+$auth = [System.Convert]::ToBase64String([char[]]$creds.GetNetworkCredential().Password)
+$auth
+$headers = @{Authorization="Basic $auth"}
+$headers
+Invoke-RestMethod -Headers $headers https://api.github.com/user
+$api = 'https://api.github.com'
+Invoke-RestMethod -Headers $headers https://api.github.com/user 
+Invoke-RestMethod -Headers $headers $api/user
+Invoke-RestMethod -Headers $headers -Method Patch $api/user -Body @{company = "AP Hogeschool - Antwerpen"}
+Invoke-RestMethod -Headers $headers -Method Patch $api/user -Body @{"company" = "AP Hogeschool - Antwerpen"}
+Invoke-RestMethod -Headers $headers -Method Patch $api/user -Body @{"company" = "AP Hogeschool - Antwerpen"}
+$body = @{"company"="AP Hogeschool - Antwerpen"}
+$body
+Invoke-RestMethod -Headers $headers -Method Patch $api/user -Body $body
+$body | ConvertFrom-Json
+$body | ConvertFrom-Json
+$body | ConvertTo-Json
+$jsonbody = ($body | ConvertTo-Json)
+$jsonbody
+Invoke-RestMethod -Headers $headers -Method Patch $api/user -Body $jsonbody
