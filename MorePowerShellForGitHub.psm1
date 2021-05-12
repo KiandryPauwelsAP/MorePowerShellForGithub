db8878916c7f7a5d20bb78aa5eb3fa84eac2541d
Kiandry Pauwels <kiandry.pauwels@student.ap.be>
2021-05-12 14:52:53 +0200

    Added help page

--- MorePowerShellForGitHub.psm1
+++ MorePowerShellForGitHub.psm1
@@ -1,4 +1,24 @@
-function Get-AuthHeader{   
+function Get-AuthHeader{
+<#
+    .SYNOPSIS
+    Gets the GitHub header to be used for authentication in other GitHub functions or custom functions.
+
+    .DESCRIPTION
+    Gets the GitHub header to be used for authentication in other GitHub functions or custom functions.
+    Takes a existing credential previously made by the user
+
+    .PARAMETER Credential
+    Specifies the credentail to be used in the header
+
+    .INPUTS
+    None
+
+    .OUTPUTS
+    A header object to be used for authentication
+
+    .EXAMPLE 
+    $header = Get-AuthHeader -Credential $creds
+#>   
     param (
         [Parameter(Mandatory=$true)]
         $Credential
@@ -9,6 +29,36 @@ function Get-AuthHeader{
 }
 
 function Add-GitHubCollaborator{
+<#
+    .SYNOPSIS
+    Sends a collaboration invite to a github user for a repository.
+
+    .DESCRIPTION
+    Sends a collaboration invite to a github user for a repository.
+    Takes a credential for authentication, a repository object and a collaborator username.
+
+    .PARAMETER Credential
+    Specifies the credentail to be used in the header.
+
+    .PARAMETER Repository
+    Specifies the repository to send an invite for.
+    This must be a Repository object gotten from Get-GitHubRepository.
+
+    .PARAMETER Collaborator
+    Specifies the username of the collaborator who will receive the invite.
+
+    .INPUTS
+    None
+
+    .OUTPUTS
+    A collaboration invite to the specified user.
+
+    .EXAMPLE 
+    Add-GitHubCollaborator -Credential $creds -Repository $repo -Collaborator "Kiandry-Pauwels"
+
+    .EXAMPLE
+    Add-GitHubCollaborator -Credential $creds -Repository $repo -Collaborator $username
+#>
     param (
     $Credential,
     $Repository,
@@ -19,4 +69,8 @@ function Add-GitHubCollaborator{
     $url = "https://api.github.com/repos"
     $repo = $Repository.full_name
     Invoke-RestMethod -Headers $headers -Method PUT -Uri $url/$repo/collaborators/$Collaborator
+}
+
+function Accept-RepositoryInvitations{
+
 }
