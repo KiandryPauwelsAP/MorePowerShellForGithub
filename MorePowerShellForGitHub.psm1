08ecc10f8cf43f1a58dbeacdc40ed9606c32f44e
Kiandry Pauwels <kiandry.pauwels@student.ap.be>
2021-05-12 14:37:14 +0200

    Added Add-GithubCollaborator function that add a collaboratot to a given repository

--- MorePowerShellForGitHub.psm1
+++ MorePowerShellForGitHub.psm1
@@ -1,8 +1,22 @@
 function Get-AuthHeader{   
     param (
+        [Parameter(Mandatory=$true)]
         $Credential
     )
     $auth = [System.Convert]::ToBase64String([char[]]$Credential.GetNetworkCredential().Password)
     $headers = @{Authorization="Basic $auth"}
-    Invoke-RestMethod -Headers $headers https://api.github.com/user
+    return $headers
+}
+
+function Add-GitHubCollaborator{
+    param (
+    $Credential,
+    $Repository,
+    $Collaborator
+    )
+    $headers = Get-AuthHeader -Credential $Credential
+    Write-Host $headers
+    $url = "https://api.github.com/repos"
+    $repo = $Repository.full_name
+    Invoke-RestMethod -Headers $headers -Method PUT -Uri $url/$repo/collaborators/$Collaborator
 }
