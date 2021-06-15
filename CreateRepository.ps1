5649ab4054b82fc84007ae5dc6d6759c74c1a99d
KiandryPauwelsAP <kiandry.pauwels@student.ap.be>
2021-06-15 16:00:18 +0200

    Initial commit

--- CreateRepository.ps1
+++ CreateRepository.ps1
@@ -1,12 +1,12 @@
 ﻿param($credential,$directory,$repository,$username)
 
 <#Authenticate to user#>
-Set-GitHubAuthentication -SessionOnly ` -Credential $credential
+<#Set-GitHubAuthentication -SessionOnly ` -Credential $credential
 $auth = [System.Convert]::ToBase64String([char[]]$credential.GetNetworkCredential().Password)
 $headers = @{Authorization="Basic $auth"}
 
 <#Create github repo#>
-$repo = New-GitHubRepository -RepositoryName $repository -Private
+<#$repo = New-GitHubRepository -RepositoryName $repository -Private
 $reponame = $repo.full_name
 Invoke-RestMethod -Headers $headers -Method PUT -Uri https://api.github.com/repos/$reponame/collaborators/dieter-ap 
 
